aad13c465ef5419ea5f28a72e5243a69eb63c87c
Ришат <risatzakirov10@gmail.com>
2024-12-25 17:55:30 +0300

    refactor(cart): optimize code for description in cart cell

--- Dodo/Screens/Cart/CartCell.swift
+++ Dodo/Screens/Cart/CartCell.swift
@@ -87,12 +87,9 @@ class CartCell: UITableViewCell {
         print("->", product.count)
         self.product = product
         nameLabel.text = product.name
-        if product.type == "pizza" {
-            descriptionLabel.update(key: product.detail ?? "", value: "\(product.size), \(product.dough), \n+\(product.ingredients ?? [])")
-        }
-        else {
-            descriptionLabel.update(key: product.detail ?? "", value: " ")
-        }
+        
+        let value = product.type == "pizza" ? "\(product.size), \(product.dough), \n+\(product.ingredients ?? [])" : ""
+        descriptionLabel.update(key: product.detail ?? "", value: value)
         priceLabel.text = "\(product.price) р."
         photoImageView.image = UIImage(named: product.image)
         
